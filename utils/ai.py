b0b0b1fb68af28c3c6a7a6dc1591c012afa14ebb
user <okoloboga37@gamil.com>
2024-11-21 09:39:45 +0300

    New OpenAI version

--- utils/ai.py
+++ utils/ai.py
@@ -6,6 +6,7 @@ import openai  # Работа с API OpenAI
 import requests  # Для синхронных HTTP-запросов
 from pathlib import Path
 from openai import OpenAI
+from openai.exceptions import ServiceUnavailableError, APIError
 from aiogram import Bot  # Для работы с ботом
 from aiogram.types.input_file import InputFile
 from midjourney_api import TNL  # Импорт библиотеки для взаимодействия с MidJourney
