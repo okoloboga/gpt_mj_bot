c76419b546a7b4848ae3d620142735e7254d5655
user <okoloboga37@gamil.com>
2024-12-23 15:28:49 +0300

    fixing all,...

--- utils/db.py
+++ utils/db.py
@@ -242,6 +242,9 @@ async def remove_chatgpt(user_id, tokens, model):
     conn: Connection = await get_conn()
     dashed_model = model.replace("-", "_")
     column = f'tokens_{model}'
+
+    logger.info('Model: {model}, column: {column}')
+
     if column not in {'tokens_4o', 'tokens_4o_mini', 'tokens_o1_preview', 'tokens_o1_mini'}:
         raise ValueError("Invalid model name")
 
