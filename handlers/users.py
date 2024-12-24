2447b33e464aa0c1b11f7a2cc560c7154ed6597d
user <okoloboga37@gamil.com>
2024-12-24 19:59:06 +0300

    if 4o-mini NO tokens -> 4o

--- handlers/users.py
+++ handlers/users.py
@@ -229,9 +229,9 @@ async def get_gpt(prompt, messages, user_id, bot: Bot, state: FSMContext):
     
     if 0 < user[f"tokens_{model_dashed}"] <= 3000:  # Если осталось 3 тыс или меньше токенов
 
-        logger.info(f"Осталось {user[f'tokens_{model_dashed}']} токенов, было уведомление: {user_notified}, было ли уведомление: {user_notified}, совершал ли покупку: {has_purchase}")
+        logger.info(f"Осталось {user[f'tokens_{model_dashed}']} токенов, было уведомление: {user_notified}, совершал ли покупку: {has_purchase}")
 
-        if user_notified is None and has_purchase is True:
+        if user_notified is None and has_purchase is False:
             logger.info(f'Скидочное уведомление')
             await db.create_user_notification_gpt(user_id)
             await notify_low_chatgpt_tokens(user_id, bot)  # Отправляем уведомление о низком количестве токенов
