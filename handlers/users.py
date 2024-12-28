570d3938af5cbaf837c7e7f5a7b3e9231356ffc9
user <okoloboga37@gamil.com>
2024-12-28 18:34:16 +0300

    docker fixing

--- handlers/users.py
+++ handlers/users.py
@@ -186,6 +186,10 @@ def split_message(text: str, max_length: int) -> list:
 
     return parts
 
+def escape_markdown_v2(text):
+    escape_chars = r'_*[]()~`>#+-=|{}.!'
+    return ''.join(['\\' + char if char in escape_chars else char for char in text])
+
 
 # Генерация ответа от ChatGPT
 async def get_gpt(prompt, messages, user_id, bot: Bot, state: FSMContext):
@@ -206,7 +210,7 @@ async def get_gpt(prompt, messages, user_id, bot: Bot, state: FSMContext):
     logger.info(f"Ответ ChatGPT: {res['content']}")
 
     if len(res["content"]) <= 4096:
-        await bot.send_message(user_id, res["content"], reply_markup=user_kb.get_clear_or_audio(), parse_mode="MarkdownV2")
+        await bot.send_message(user_id, escape_markdown_v2(res["content"]), reply_markup=user_kb.get_clear_or_audio(), parse_mode="MarkdownV2")
     else:
         # Разделение сообщения на части
         parts = split_message(res["content"], 4096)
